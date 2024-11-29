88f0d9684b20f2e4240e992a5cdc8f09666cacb0
Pedro Alexandre <163411746+Pedro-Alexandr@users.noreply.github.com>
2024-11-29 11:34:33 -0300

    Add files via upload

---
+++ Remob - Back-end/remob/src/main/java/br/com/senac/remob/model/Passageiro.java
@@ -0,0 +1,105 @@
+package br.com.senac.remob.model;
+
+import java. util. Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "passageiro")
+public class Passageiro extends Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String cpf;
+
+    @Column
+    private Date idade;
+
+    @Column
+    private int telefone;
+
+    @Column
+    private String email;
+
+    @Column
+    private String senha;
+
+    @Column
+    private String sexo;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Date idade) {
+        this.idade = idade;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
